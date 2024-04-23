a24c166667fcb263f13b7f9cedc6920e6eadfd13
Basith Ahmed <143781280+Basith-Ahmed@users.noreply.github.com>
2024-04-24 00:26:37 +0530

    Update zombiehelper.sol

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -16,8 +16,8 @@ contract ZombieHelper is ZombieFeeding {
     _owner.transfer(address(this).balance);
   }
 
-  function setLevelUpFee (uint _fee) external onlyOwner{
-    levelUpFee == _fee;
+  function setLevelUpFee(uint _fee) external onlyOwner{
+    levelUpFee = _fee;
   }
 
   function levelUp(uint _zombieId) external payable {
